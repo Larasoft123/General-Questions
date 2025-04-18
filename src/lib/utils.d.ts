640e65ffa012343cdeee9210d13141a70269c634
Moises Lara <Moisesdavidlara1@gamil.com>
2025-04-18 12:10:16 -0400

    Agregar preguntas de capitales de países al arreglo de preguntas y actualizar opciones iniciales

--- src/lib/utils.d.ts
+++ src/lib/utils.d.ts
@@ -1,209 +1,496 @@
 import { Question } from "./types";
 
 export const questionsArray: Question[] = [
+  {
+    question: "¿Cuál es la capital de Afganistán?",
+    answers: ["Kabul", "Kandahar", "Herat", "Islamabad"],
+    correct: 0,
+  },
+  {
+    question: "¿Cuál es la capital de Albania?",
+    answers: ["Durrës", "Vlorë", "Tirana", "Skopie"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Alemania?",
+    answers: ["Múnich", "Hamburgo", "Fráncfort", "Berlín"],
+    correct: 3,
+  },
+  {
+    question: "¿Cuál es la capital de Andorra?",
+    answers: ["Encamp", "Andorra la Vieja", "La Massana", "Tolosa"],
+    correct: 1,
+  },
+  {
+    question: "¿Cuál es la capital de Angola?",
+    answers: ["Huambo", "Luanda", "Lobito", "Kinsasa"],
+    correct: 1,
+  },
+  {
+    question: "¿Cuál es la capital de Arabia Saudita?",
+    answers: ["La Meca", "Medina", "Riad", "Yeda"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Argelia?",
+    answers: ["Orán", "Constantina", "Argel", "Túnez"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Argentina?",
+    answers: ["Córdoba", "Rosario", "Buenos Aires", "Montevideo"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Armenia?",
+    answers: ["Giumri", "Ereván", "Vanadzor", "Tiflis"],
+    correct: 1,
+  },
+  {
+    question: "¿Cuál es la capital de Australia?",
+    answers: ["Sídney", "Melbourne", "Brisbane", "Canberra"],
+    correct: 3,
+  },
+  {
+    question: "¿Cuál es la capital de Austria?",
+    answers: ["Salzburgo", "Innsbruck", "Viena", "Graz"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Azerbaiyán?",
+    answers: ["Ganja", "Sumqayit", "Bakú", "Ereván"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Bahamas?",
+    answers: ["Freeport", "Nasáu", "West End", "La Habana"],
+    correct: 1,
+  },
+  {
+    question: "¿Cuál es la capital de Bangladés?",
+    answers: ["Chittagong", "Khulna", "Daca", "Calcuta"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Barbados?",
+    answers: ["Speightstown", "Oistins", "Bridgetown", "Holetown"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Baréin?",
+    answers: ["Manama", "Riffa", "Muharraq", "Doha"],
+    correct: 0,
+  },
+  {
+    question: "¿Cuál es la capital de Bélgica?",
+    answers: ["Amberes", "Gante", "Charleroi", "Bruselas"],
+    correct: 3,
+  },
+  {
+    question: "¿Cuál es la capital de Belice?",
+    answers: ["Ciudad de Belice", "San Ignacio", "Belmopán", "Orange Walk"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Benín?",
+    answers: ["Cotonú", "Porto Novo", "Abomey-Calavi", "Lomé"],
+    correct: 1,
+  },
+  {
+    question: "¿Cuál es la capital de Bielorrusia?",
+    answers: ["Gómel", "Minsk", "Maguilov", "Kiev"],
+    correct: 1,
+  },
+  {
+    question: "¿Cuál es la capital de Birmania (Myanmar)?",
+    answers: ["Rangún", "Mandalay", "Naipyidó", "Bangkok"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Bolivia?",
+    answers: ["Santa Cruz", "La Paz", "Sucre", "Cochabamba"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Bosnia y Herzegovina?",
+    answers: ["Bania Luka", "Mostar", "Sarajevo", "Belgrado"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Botsuana?",
+    answers: ["Francistown", "Gaborone", "Molepolole", "Harare"],
+    correct: 1,
+  },
+  {
+    question: "¿Cuál es la capital de Brasil?",
+    answers: ["Río de Janeiro", "São Paulo", "Salvador", "Brasilia"],
+    correct: 3,
+  },
+  {
+    question: "¿Cuál es la capital de Brunéi?",
+    answers: ["Kuala Belait", "Seria", "Bandar Seri Begawan", "Kota Kinabalu"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Bulgaria?",
+    answers: ["Plovdiv", "Varna", "Sofía", "Bucarest"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Burkina Faso?",
+    answers: ["Bobo-Dioulasso", "Koudougou", "Uagadugú", "Acra"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Burundi?",
+    answers: ["Buyumbura", "Gitega", "Ngozi", "Kigali"],
+    correct: 1,
+  },
+  {
+    question: "¿Cuál es la capital de Bután?",
+    answers: ["Paro", "Punakha", "Timbu", "Katmandú"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Cabo Verde?",
+    answers: ["Mindelo", "Praia", "Santa María", "Dakar"],
+    correct: 1,
+  },
+  {
+    question: "¿Cuál es la capital de Camboya?",
+    answers: ["Siem Reap", "Battambang", "Phnom Penh", "Bangkok"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Camerún?",
+    answers: ["Duala", "Yaundé", "Garoua", "Abuya"],
+    correct: 1,
+  },
+  {
+    question: "¿Cuál es la capital de Canadá?",
+    answers: ["Toronto", "Montreal", "Vancouver", "Ottawa"],
+    correct: 3,
+  },
+  {
+    question: "¿Cuál es la capital de Catar?",
+    answers: ["Rayán", "Doha", "Al Wakrah", "Manama"],
+    correct: 1,
+  },
+  {
+    question: "¿Cuál es la capital de Chad?",
+    answers: ["Moundou", "Sarh", "Yamena", "Niamey"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Chile?",
+    answers: ["Valparaíso", "Concepción", "Santiago", "Buenos Aires"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de China?",
+    answers: ["Shanghái", "Pekín", "Cantón", "Hong Kong"],
+    correct: 1,
+  },
+  {
+    question: "¿Cuál es la capital de Chipre?",
+    answers: ["Limassol", "Lárnaca", "Nicosia", "Atenas"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Colombia?",
+    answers: ["Medellín", "Cali", "Bogotá", "Caracas"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Comoras?",
+    answers: ["Mutsamudu", "Moroni", "Domoni", "Antananarivo"],
+    correct: 1,
+  },
+  {
+    question: "¿Cuál es la capital de Corea del Norte?",
+    answers: ["Hamhung", "Chongjin", "Pionyang", "Seúl"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Corea del Sur?",
+    answers: ["Busan", "Incheon", "Daegu", "Seúl"],
+    correct: 3,
+  },
+  {
+    question: "¿Cuál es la capital de Costa de Marfil?",
+    answers: ["Abiyán", "Yamusukro", "Bouaké", "Acra"],
+    correct: 1,
+  },
+  {
+    question: "¿Cuál es la capital de Costa Rica?",
+    answers: ["Limón", "San José", "Alajuela", "Panamá"],
+    correct: 1,
+  },
+  {
+    question: "¿Cuál es la capital de Croacia?",
+    answers: ["Split", "Rijeka", "Zagreb", "Liubliana"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Cuba?",
+    answers: ["Santiago de Cuba", "Camagüey", "La Habana", "Miami"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Dinamarca?",
+    answers: ["Aarhus", "Odense", "Copenhague", "Estocolmo"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Yibuti?",
+    answers: ["Ali Sabieh", "Tadjoura", "Yibuti", "Adís Abeba"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Dominica?",
+    answers: ["Portsmouth", "Roseau", "Marigot", "Saint George"],
+    correct: 1,
+  },
+  {
+    question: "¿Cuál es la capital de Ecuador?",
+    answers: ["Guayaquil", "Cuenca", "Quito", "Lima"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Egipto?",
+    answers: ["Alejandría", "Guiza", "El Cairo", "Luxor"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de El Salvador?",
+    answers: ["Santa Ana", "San Salvador", "San Miguel", "Tegucigalpa"],
+    correct: 1,
+  },
+  {
+    question: "¿Cuál es la capital de Emiratos Árabes Unidos?",
+    answers: ["Dubái", "Sharjah", "Abu Dabi", "Mascate"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Eritrea?",
+    answers: ["Keren", "Asmara", "Massawa", "Jartum"],
+    correct: 1,
+  },
+  {
+    question: "¿Cuál es la capital de Eslovaquia?",
+    answers: ["Košice", "Prešov", "Bratislava", "Viena"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Eslovenia?",
+    answers: ["Maribor", "Celje", "Liubliana", "Zagreb"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de España?",
+    answers: ["Barcelona", "Valencia", "Sevilla", "Madrid"],
+    correct: 3,
+  },
+  {
+    question: "¿Cuál es la capital de Estados Unidos?",
+    answers: ["Nueva York", "Los Ángeles", "Chicago", "Washington D. C."],
+    correct: 3,
+  },
+  {
+    question: "¿Cuál es la capital de Estonia?",
+    answers: ["Tartu", "Narva", "Tallin", "Riga"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Esuatini (Suazilandia)?",
+    answers: ["Manzini", "Mbabane", "Lobamba", "Maputo"],
+    correct: 1,
+  },
+  {
+    question: "¿Cuál es la capital de Etiopía?",
+    answers: ["Dire Dawa", "Adís Abeba", "Mekele", "Nairobi"],
+    correct: 1,
+  },
+  {
+    question: "¿Cuál es la capital de Filipinas?",
+    answers: ["Ciudad Quezón", "Dávao", "Manila", "Cebú"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Finlandia?",
+    answers: ["Espoo", "Tampere", "Helsinki", "Turku"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Fiyi?",
+    answers: ["Lautoka", "Nadi", "Suva", "Apia"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Francia?",
+    answers: ["Marsella", "Lyon", "París", "Burdeos"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Gabón?",
+    answers: ["Port-Gentil", "Franceville", "Libreville", "Yaundé"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Gambia?",
+    answers: ["Serekunda", "Brikama", "Banjul", "Dakar"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Georgia?",
+    answers: ["Kutaisi", "Batumi", "Tiflis", "Ereván"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Ghana?",
+    answers: ["Kumasi", "Tamale", "Acra", "Lomé"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Granada?",
+    answers: ["Gouyave", "Saint George", "Victoria", "Bridgetown"],
+    correct: 1,
+  },
+  {
+    question: "¿Cuál es la capital de Grecia?",
+    answers: ["Salónica", "Patras", "Heraclión", "Atenas"],
+    correct: 3,
+  },
+  {
+    question: "¿Cuál es la capital de Guatemala?",
+    answers: ["Mixco", "Villa Nueva", "Ciudad de Guatemala", "San Salvador"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Guinea?",
+    answers: ["Nzérékoré", "Conakri", "Kankan", "Freetown"],
+    correct: 1,
+  },
+  {
+    question: "¿Cuál es la capital de Guinea-Bisáu?",
+    answers: ["Bafatá", "Gabú", "Bisáu", "Conakri"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Guinea Ecuatorial?",
+    answers: ["Bata", "Malabo", "Ebebiyín", "Libreville"],
+    correct: 1,
+  },
+  {
+    question: "¿Cuál es la capital de Guyana?",
+    answers: ["Linden", "Georgetown", "Nueva Ámsterdam", "Paramaribo"],
+    correct: 1,
+  },
+  {
+    question: "¿Cuál es la capital de Haití?",
+    answers: ["Carrefour", "Delmas", "Puerto Príncipe", "Santo Domingo"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Honduras?",
+    answers: ["San Pedro Sula", "Tegucigalpa", "La Ceiba", "Managua"],
+    correct: 1,
+  },
+  {
+    question: "¿Cuál es la capital de Hungría?",
+    answers: ["Debrecen", "Szeged", "Budapest", "Miskolc"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de India?",
+    answers: ["Bombay", "Bangalore", "Nueva Delhi", "Calcuta"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Indonesia?",
+    answers: ["Surabaya", "Yakarta", "Bandung", "Medan"],
+    correct: 1,
+  },
+  {
+    question: "¿Cuál es la capital de Irak?",
+    answers: ["Basora", "Mosul", "Bagdad", "Erbil"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Irán?",
+    answers: ["Mashhad", "Isfahán", "Teherán", "Shiraz"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Irlanda?",
+    answers: ["Cork", "Limerick", "Galway", "Dublín"],
+    correct: 3,
+  },
+  {
+    question: "¿Cuál es la capital de Islandia?",
+    answers: ["Kópavogur", "Hafnarfjörður", "Reikiavik", "Akureyri"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Israel?",
+    answers: ["Tel Aviv", "Haifa", "Jerusalén", "Rishon LeZion"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Italia?",
+    answers: ["Milán", "Nápoles", "Turín", "Roma"],
+    correct: 3,
+  },
+  {
+    question: "¿Cuál es la capital de Jamaica?",
+    answers: ["Montego Bay", "Spanish Town", "Kingston", "Portmore"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Japón?",
+    answers: ["Osaka", "Kioto", "Yokohama", "Tokio"],
+    correct: 3,
+  },
+  {
+    question: "¿Cuál es la capital de Jordania?",
+    answers: ["Zarqa", "Irbid", "Amán", "Russeifa"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Kazajistán?",
+    answers: ["Almaty", "Nursultán (Astaná)", "Shymkent", "Karagandá"],
+    correct: 1,
+  },
+  {
+    question: "¿Cuál es la capital de Kenia?",
+    answers: ["Mombasa", "Kisumu", "Nairobi", "Nakuru"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Kirguistán?",
+    answers: ["Osh", "Biskek", "Jalal-Abad", "Dusambé"],
+    correct: 1,
+  },
+  {
+    question: "¿Cuál es la capital de Kiribati?",
+    answers: ["Betio", "Bikenibeu", "Tarawa Sur", "Funafuti"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Kuwait?",
+    answers: ["Hawalli", "Salmiya", "Ciudad de Kuwait", "Farwaniya"],
+    correct: 2,
+  },
+  {
+    question: "¿Cuál es la capital de Laos?",
+    answers: ["Luang Prabang", "Savannakhet", "Vientián", "Pakse"],
+    correct: 2,
+  },
+];
 
-    {
-      "question": "¿Cuál es la capital de España?",
-      "answers": [
-        "Barcelona",
-        "Madrid",
-        "Sevilla",
-        "Valencia"
-      ],
-      "correct": 1
-    },
-    {
-      "question": "¿Cuál es la capital de Vietnam?",
-      "answers": [
-        "Ho Chi Minh",
-        "Hanoi",
-        "Da Nang",
-        "Hue"
-      ],
-      "correct": 1
-    },
-    {
-      "question": "¿Cuál es la capital de Suiza?",
-      "answers": [
-        "Zúrich",
-        "Ginebra",
-        "Berna",
-        "Lausana"
-      ],
-      "correct": 2
-    },
-    {
-      "question": "¿Cuál es la capital de Chile?",
-      "answers": [
-        "Valparaíso",
-        "Concepción",
-        "Santiago",
-        "Antofagasta"
-      ],
-      "correct": 2
-    },
-    {
-      "question": "¿Cuál es la capital de Kenia?",
-      "answers": [
-        "Mombasa",
-        "Nairobi",
-        "Kisumu",
-        "Nakuru"
-      ],
-      "correct": 1
-    },
-    {
-      "question": "¿Cuál es la capital de Portugal?",
-      "answers": [
-        "Oporto",
-        "Lisboa",
-        "Braga",
-        "Faro"
-      ],
-      "correct": 1
-    },
-    {
-      "question": "¿Cuál es la capital de Polonia?",
-      "answers": [
-        "Cracovia",
-        "Łódź",
-        "Varsovia",
-        "Breslavia"
-      ],
-      "correct": 2
-    },
-    {
-      "question": "¿Cuál es la capital de Irán?",
-      "answers": [
-        "Isfahán",
-        "Shiraz",
-        "Teherán",
-        "Tabriz"
-      ],
-      "correct": 2
-    },
-    {
-      "question": "¿Cuál es la capital de Países Bajos?",
-      "answers": [
-        "Rotterdam",
-        "La Haya",
-        "Ámsterdam",
-        "Utrecht"
-      ],
-      "correct": 2
-    },
-    {
-      "question": "¿Cuál es la capital de Filipinas?",
-      "answers": [
-        "Cebú",
-        "Davao",
-        "Manila",
-        "Quezon City"
-      ],
-      "correct": 2
-    },
-    {
-      "question": "¿Cuál es la capital de Austria?",
-      "answers": [
-        "Graz",
-        "Viena",
-        "Salzburgo",
-        "Innsbruck"
-      ],
-      "correct": 1
-    },
-    {
-      "question": "¿Cuál es la capital de Marruecos?",
-      "answers": [
-        "Casablanca",
-        "Marrakech",
-        "Rabat",
-        "Fez"
-      ],
-      "correct": 2
-    },
-    {
-      "question": "¿Cuál es la capital de Ucrania?",
-      "answers": [
-        "Járkov",
-        "Odesa",
-        "Kiev",
-        "Leópolis"
-      ],
-      "correct": 2
-    },
-    {
-      "question": "¿Cuál es la capital de Malasia?",
-      "answers": [
-        "Ipoh",
-        "Johor Bahru",
-        "Kuala Lumpur",
-        "Penang"
-      ],
-      "correct": 2
-    },
-    {
-      "question": "¿Cuál es la capital de Sudán?",
-      "answers": [
-        "Omdurmán",
-        "Puerto Sudán",
-        "Jartum",
-        "Kassala"
-      ],
-      "correct": 2
-    },
-    {
-      "question": "¿Cuál es la capital de Venezuela?",
-      "answers": [
-        "Maracaibo",
-        "Valencia",
-        "Caracas",
-        "Barquisimeto"
-      ],
-      "correct": 2
-    },
-    {
-      "question": "¿Cuál es la capital de Nueva Zelanda?",
-      "answers": [
-        "Auckland",
-        "Christchurch",
-        "Wellington",
-        "Hamilton"
-      ],
-      "correct": 2
-    },
-    {
-      "question": "¿Cuál es la capital de Dinamarca?",
-      "answers": [
-        "Aarhus",
-        "Odense",
-        "Copenhague",
-        "Aalborg"
-      ],
-      "correct": 2
-    },
-    {
-      "question": "¿Cuál es la capital de Irlanda?",
-      "answers": [
-        "Cork",
-        "Galway",
-        "Dublín",
-        "Limerick"
-      ],
-      "correct": 2
-    },
-    {
-      "question": "¿Cuál es la capital de Singapur?",
-      "answers": [
-        "Singapur",
-        "Johor Bahru",
-        "Kuala Lumpur",
-        "Batán"
-      ],
-      "correct": 0
-    }
-  ]
-
-
-
-export const optionsInit= [3,5,10,15,questionsArray.length]
+export const optionsInit = [
+  Math.round(questionsArray.length / 4),
+  Math.round(questionsArray.length / 3),
+  Math.round(questionsArray.length / 2),
+  questionsArray.length,
+];
