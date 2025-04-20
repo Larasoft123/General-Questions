48325890d12ae12b7d8dbb618b0420f0ea50da55
Moises Lara <Moisesdavidlara1@gamil.com>
2025-04-20 12:17:53 -0400

    Agregar lógica de reinicio y mejorar el componente Finish con detalles de respuestas incorrectas

--- src/components/Finish.tsx
+++ src/components/Finish.tsx
@@ -1,18 +1,33 @@
+
 import { useQuestionsStore } from "../store/questionsStore"
+import { Reset } from "./reset"
+
 
 export function Finish() {
 
     const win = useQuestionsStore(state => state.win)
+    const questions = useQuestionsStore(state => state.questions)
     
+
 
     const message = win ? "felicidades superaste la prueba" : "Esfuerzate más, no contestaste bien la mitad de las preguntas"
 
     return (
         <>
-            {win !== null && <div className="flex px-4 justify-center text-center items-center w-full h-full">
-                <h1 className={`-translate-y-12 text-3xl text-balance text-teal-700`}> {message} </h1>
-            </div>
+            {win !== null && <section className="px-4  text-center py-90 ">
+
+
+                <h1 className={` text-3xl text-balance mb-8 text-teal-700`}> {message} </h1>
+
+                <h5 className="text-lg">{questions.filter((q) => q.userAnswer !== undefined && q.userAnswer !== q.correct).length}/{questions.length} Preguntas incorrectas</h5>
+
+
+                <div className="p-4 flex justify-center max-w-md items-center mx-auto">
+                    <Reset />
+                </div>
+            </section>
             }
+
         </>
     )
 
