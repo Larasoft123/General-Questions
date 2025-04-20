39c5036dec46d7db9eb4cbb3cc50e91bd85f25a4
Moises Lara <Moisesdavidlara1@gamil.com>
2025-04-20 12:17:35 -0400

    Refactor componente Asking: elimina lógica de manejo de respuestas y simplifica renderizado de opciones

--- src/components/Asking.tsx
+++ src/components/Asking.tsx
@@ -1,35 +1,19 @@
 
-import { Question } from "../lib/types"
 import { useQuestionsStore } from "../store/questionsStore"
 import { NextButtons } from "./NextButtons"
 import { Footer } from "./Footer"
+import { QuestionSelect } from "./question-select"
 
 
-
-function getBg({ question, index }: { question: Question, index: number }) {
-    const { correct, userAnswer } = question
-    if (userAnswer == undefined) return "transparent"
-
-    if (correct === index) return "bg-green-500"
-
-    if (index === userAnswer) return "bg-red-500"
-
-    return "transparent"
-
-}
 
 export function Asking() {
     const currentQuestion = useQuestionsStore(state => state.currentQuestion)
     const questions = useQuestionsStore(state => state.questions)
-    const setUserAnswer = useQuestionsStore(state => state.setUserAnswer)
 
 
 
 
-    const handleClick = (index: number) => () => {
-        setUserAnswer(index)
-    }
-
+ 
     return (
         <section className="w-full p-4 h-full flex items-center justify-center">
 
@@ -47,13 +31,8 @@ export function Asking() {
                     <div className="">
                         <ul className="flex flex-col gap-4">
                             {questions[currentQuestion].answers.map((answer, index) => {
-
                                 return (
-                                    <li key={index} className={`flex flex-col gap-2 text-center`}>
-                                        <button disabled={questions[currentQuestion].userAnswer !== undefined} onClick={handleClick(index)} className={`${getBg({ index, question: questions[currentQuestion] })} bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}>
-                                            {answer}
-                                        </button>
-                                    </li>
+                                    <QuestionSelect key={index} answer={answer} index={index} />
                                 )
                             })}
 
