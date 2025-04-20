c66321eb1bc7ea9e3a4b91268d7a701a72841eac
Moises Lara <Moisesdavidlara1@gamil.com>
2025-04-20 12:28:28 -0400

    Agregar persistencia al store de preguntas y mejorar la lógica de reinicio

--- src/store/questionsStore.ts
+++ src/store/questionsStore.ts
@@ -3,6 +3,7 @@ import { questionsArray } from "../lib/utils.d";
 import { Question, App_status } from "../lib/types.d";
 import confetti from "canvas-confetti";
 import { devtools } from "zustand/middleware";
+import { persist } from "zustand/middleware";
 
 interface QuestionsStore {
   currentQuestion: number;
@@ -45,90 +46,89 @@ function DoConffeti() {
   })();
 }
 
-export const useQuestionsStore = create<QuestionsStore>()(devtools((set, get) => ({
-  currentQuestion: 0,
-  questions: [],
-  App_status: App_status.INIT,
-  win: null,
-
-  setQuestions(nroQuestions: number) {
-    const newQuestions = structuredClone(questionsArray)
-      .sort(() => Math.random() - 0.5)
-      .slice(0, nroQuestions);
-
-    // setear el array questions con el numero de preguntas que se le pase y hacerlo al azar
-    set({
-      questions: newQuestions,
-      App_status: App_status.ASKING,
-    });
-  },
-
-  checkWin() {
-    const questions = get().questions;
-    const totalQuestions = questions.length;
-
-    const correctQuestions = questions.filter(
-      (q) => q.userAnswer === q.correct
-    );
-
- 
-
-    if (correctQuestions.length >= Math.round(totalQuestions / 2)) {
-        console.log("seteado win")
-        DoConffeti();
-        set({ win: true });
-      return;
-    }
-
-    
-    return set({win: false});
-
-
-  },
-
-  setUserAnswer(answer: number) {
-    const question = get().questions[get().currentQuestion];
-    question.userAnswer = answer;
-
-    set({
-      questions: get().questions.map((q, index) => {
-        return index === get().currentQuestion ? question : q;
-      }),
-    });
-
-    if (get().currentQuestion == get().questions.length - 1) {
-      get().checkWin();
-
-     return set({App_status: App_status.FINISH});
-  
-
-    }
-
-    setTimeout(() => {
-      set({
-        currentQuestion: get().currentQuestion + 1,
-      });
-    }, 1000);
-  },
-
-
-  reset(){
-    set({
+export const useQuestionsStore = create<QuestionsStore>()(
+  persist(
+    devtools((set, get) => ({
       currentQuestion: 0,
       questions: [],
       App_status: App_status.INIT,
       win: null,
-    });
-  },
 
-  GoNext() {
-    if (get().currentQuestion == get().questions.length - 1) return;
+      setQuestions(nroQuestions: number) {
+        const newQuestions = structuredClone(questionsArray)
+          .sort(() => Math.random() - 0.5)
+          .slice(0, nroQuestions);
 
-    set({ currentQuestion: get().currentQuestion + 1 });
-  },
+        // setear el array questions con el numero de preguntas que se le pase y hacerlo al azar
+        set({
+          questions: newQuestions,
+          App_status: App_status.ASKING,
+        });
+      },
 
-  GoBack() {
-    if (get().currentQuestion == 0) return;
-    set({ currentQuestion: get().currentQuestion - 1 });
-  },
-})));
+      checkWin() {
+        const questions = get().questions;
+        const totalQuestions = questions.length;
+
+        const correctQuestions = questions.filter(
+          (q) => q.userAnswer === q.correct
+        );
+
+        if (correctQuestions.length >= Math.round(totalQuestions / 2)) {
+          console.log("seteado win");
+          DoConffeti();
+          set({ win: true });
+          return;
+        }
+
+        return set({ win: false });
+      },
+
+      setUserAnswer(answer: number) {
+        const question = get().questions[get().currentQuestion];
+        question.userAnswer = answer;
+
+        set({
+          questions: get().questions.map((q, index) => {
+            return index === get().currentQuestion ? question : q;
+          }),
+        });
+
+        if (get().currentQuestion == get().questions.length - 1) {
+          get().checkWin();
+
+          return set({ App_status: App_status.FINISH });
+        }
+
+        setTimeout(() => {
+          set({
+            currentQuestion: get().currentQuestion + 1,
+          });
+        }, 1000);
+      },
+
+      reset() {
+        set({
+          currentQuestion: 0,
+          questions: [],
+          App_status: App_status.INIT,
+          win: null,
+        });
+      },
+
+      GoNext() {
+        if (get().currentQuestion == get().questions.length - 1) return;
+
+        set({ currentQuestion: get().currentQuestion + 1 });
+      },
+
+      GoBack() {
+        if (get().currentQuestion == 0) return;
+        set({ currentQuestion: get().currentQuestion - 1 });
+      },
+    })), {
+      name: "questionsStore", // unique name
+      // (optional) by default the 'localStorage' is used
+    }
+  )
+);
