e164082735ea5288cf8b41b58a5ec30cae8fe215
Moises Lara <Moisesdavidlara1@gamil.com>
2025-04-21 14:35:14 -0400

    Agregar el componente Toaster y mostrar ChooseQuestions en el estado de selección de tipo de preguntas

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { App_status } from "./lib/types.d"
 import { Init } from "./components/init"
 import { Asking } from "./components/Asking"
 import { Finish } from "./components/Finish"
+import { Toaster } from "sonner"
+import { ChooseQuestions } from "./components/chosee-questions"
 
 
 
@@ -24,7 +26,10 @@ function App() {
 
   return (
     <div className="App w-full h-screen flex flex-col items-center">
+
+      <Toaster />
       <main className='max-w-4xl h-full mx-auto px-4 py-2 w-full text-balance'>
+        {APP_STATUS == App_status.CHOSE_TYPE_QUESTIONS && <ChooseQuestions />}
         {APP_STATUS == App_status.INIT && <Init />}
         {APP_STATUS == App_status.ASKING && <Asking />}
         {APP_STATUS == App_status.FINISH && <Finish />}
