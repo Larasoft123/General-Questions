5e1aeaa3aec4991cfab13a353bc3269b81f37519
Moises Lara <Moisesdavidlara1@gamil.com>
2025-04-21 14:34:38 -0400

    Agregar tipo a la interfaz Question y mejorar la enumeración App_status

--- src/lib/types.d.ts
+++ src/lib/types.d.ts
@@ -1,12 +1,23 @@
 export interface Question {
   question: string,
   answers: string[],
+  type: string,
   correct: number
   userAnswer?: number,
+  
 };
 
 export enum App_status{
+  CHOSE_TYPE_QUESTIONS = "CHOSE_TYPE_QUESTIONS",
   INIT = "INIT",
   FINISH = "FINISH",
   ASKING = "ASKING",
 }
+
+
+
+export const typeQuestions = {
+  GEO: "Geografía",
+  SCIENCE: "Ciencia",
+
+} as const
