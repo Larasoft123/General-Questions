63e8b8d2c9c560da98b7e10503424252bdb6d737
Moises Lara <Moisesdavidlara1@gamil.com>
2025-04-25 19:01:47 -0400

    Reorganizar el contenido del pie de página para mejorar la legibilidad y el estilo de los contadores de preguntas

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,11 +6,15 @@ export function Footer() {
 
     return (
         <footer className="flex justify-center gap-8 items-center  text-xs   text-gray-700">
-            <div>
-                Correctas: {questions.filter(q => q.userAnswer === q.correct).length}/{questions.length}
+            <div className="text-indigo-700">
+                Sin contestar: {questions.filter(q => q.userAnswer === undefined).length}/{questions.length}
             </div>
             
-            <div>
+            <div className="text-emerald-700">
+                Correctas: {questions.filter(q => q.userAnswer === q.correct).length}/{questions.length}
+            </div>
+
+            <div className="text-red-600">
                 Incorrectas: {questions.filter(q => q.userAnswer !== undefined && q.userAnswer !== q.correct).length}/{questions.length}
             </div>
 
