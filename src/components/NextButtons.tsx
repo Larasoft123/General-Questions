7ad4d422390448b60b75b3b4b85d0e287dbc0131
Moises Lara <Moisesdavidlara1@gamil.com>
2025-04-27 13:48:37 -0400

    Agregar etiquetas aria a los botones de navegación para mejorar la accesibilidad

--- src/components/NextButtons.tsx
+++ src/components/NextButtons.tsx
@@ -11,10 +11,10 @@ export function NextButtons() {
         <>
 
             <div className="flex gap-8">
-                <button onClick={GoBack} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <button aria-label="Ir a la pregunta anterior" role="button"  onClick={GoBack} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                     <GoBackIcon />
                 </button>
-                <button onClick={GoNext} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <button aria-label="ir a la pregunta siguiente" role="button"  onClick={GoNext} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                     <GoNextIcon />
                 </button>
             </div>
