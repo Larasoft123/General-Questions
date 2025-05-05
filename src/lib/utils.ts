04a40bb2b8817aac0f05575a344946e08d6ee720
Moises <moisesdavidlara1@gmail.com>
2025-05-05 18:32:29 -0400

    agregar arreglo de preguntas de historia de Venezuela

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,9 +2,7 @@ import { biologyArray } from "./biology-questions";
 import { scienceArray } from "./science-questions";
 import { GeoArray } from "./geo-questions";
 import { historyArray } from "./history-questions";
-
-import { shuffleAnswers } from "./shuffleAnswers";
-
+import { vzlaHistoryArray } from "./vzla-history-questions";
 
 
 
@@ -12,15 +10,11 @@ import { shuffleAnswers } from "./shuffleAnswers";
 
 
 
-export const totalQuestions = [...GeoArray,...historyArray, ...scienceArray, ...biologyArray];
 
 
-// shuffle the answers of the total questions array
-export const newTotalQuestions = totalQuestions.map((question) => {
-    const newQuestion = shuffleAnswers({question})
-    
-    return newQuestion
-})
+
+export const totalQuestions = [...GeoArray,...historyArray, ...scienceArray, ...biologyArray, ...vzlaHistoryArray];
+
 
 
 
